11f81ee889c1b0c195a9d13a6028b5741fa37032
boertel <benjamin.oertel@gmail.com>
2018-06-14 13:17:20 -0700

    better sort

--- src/ui/Leaderboard.js
+++ src/ui/Leaderboard.js
@@ -14,7 +14,7 @@ const User = connect((state, ownProps) => ({...state.user[ownProps.id]}))(withCl
 
 
 const Leaderboard = ({ users, currentUserId, className, children, }) => {
-  const leaderboard = sortBy(values(users), ['points', 'full_name']).reverse();
+  const leaderboard = sortBy(sortBy(values(users), 'full_name').reverse(), 'points').reverse()
   let rank = 0;
   let previousPoints = -1;
   const title = children ? <h3>Leaderboard {children} ({leaderboard.length})</h3> : <h3>Leaderboard  ({leaderboard.length})</h3>
