4c358ed81df119ea66456d6365bb0c3539707822
boertel <benjamin.oertel@gmail.com>
2018-06-15 11:41:37 -0700

    color BetItem

--- src/views/Bets.js
+++ src/views/Bets.js
@@ -8,7 +8,7 @@ import { get } from 'lodash';
 import { asyncConnect } from '../utils/components';
 import { withClassNames } from '../ui/utils';
 import { loadBets } from '../resources/bet';
-import { ProxyComponent, proxy } from '../resources/utils';
+import { ProxyComponent, proxy, getStatus } from '../resources/utils';
 import SelectFriends from '../views/SelectFriends';
 
 
@@ -29,12 +29,15 @@ const sortScoreTie = (a, b) => {
 }
 
 
-const BetItem = connect((state, ownProps) => ({user: state.user[ownProps.user.id], currentUserId: state.auth.id,}))(withClassNames('bet-item', props => props.currentUserId === props.user.id && ' me')(({ user, currentUserId, score_a, score_b, className }) => (
-  <div className={className}>
-    <div>{user.full_name}</div>
-    <div>{score_a} - {score_b}</div>
-  </div>
-)))
+const BetItem = connect((state, ownProps) => ({user: state.user[ownProps.user.id], currentUserId: state.auth.id,}))(withClassNames('bet-item', props => props.currentUserId === props.user.id && ' me')(({ user, currentUserId, score_a, score_b, className, game, }) => {
+  const betStatus = getStatus(game.score_a, game_score_b, { score_a, score_b });
+  return (
+    <div className={`${className} ${betStatus}`}>
+      <div>{user.full_name}</div>
+      <div>{score_a} - {score_b}</div>
+    </div>
+  )
+}))
 
 const BetSection = ({ title, bets, filter, sort, }) => (
   <div className="bet-section">
